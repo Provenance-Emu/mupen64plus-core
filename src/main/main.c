34636f268e9e7201264c565b87cccdc1ef982578
Joseph Mattiello <mail@joemattiello.com>
2018-04-03 00:08:25 -0400

    Add macro to remove new_vi
    We have our own timing, this slows us down a lot even if i add the interupt video callback, we get back stutters cause of the pause_loop()

--- src/main/main.c
+++ src/main/main.c
@@ -952,6 +952,7 @@ static void pause_loop(void)
 
 /* called on vertical interrupt.
  * Allow the core to perform various things */
+#ifndef IN_OPENEMU
 void new_vi(void)
 {
 #if defined(PROFILE)
@@ -967,6 +968,7 @@ void new_vi(void)
 
     netplay_check_sync(&g_dev.r4300.cp0);
 }
+#endif
 
 static void main_switch_pak(int control_id)
 {
